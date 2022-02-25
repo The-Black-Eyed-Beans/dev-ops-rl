28a33621a9adda7b3da3789c1e3dc69e4631d1be
Ricky-Lopez <rllopez718@gmail.com>
2022-02-25 02:42:23 -0500

    began writing IAAC for ECS deployment

---
+++ terraform/EKS/variables.tf
@@ -0,0 +1,58 @@
+#Network Vars
+variable "VPC_cidr_block" {
+    type = string
+    description = "CIDR block used by VPC instance."
+    default = "10.1.0.0/16"
+}
+
+variable "private_subnet_cidr_block" {
+    type = string
+    description = ""
+    default = "10.1.1.0/24"
+}
+
+variable "public_subnet_cidr_block" {
+    type = string
+    description = ""
+    default = "10.1.2.0/24"
+}
+
+variable "public_subnet_availability_zone" {
+    type = string
+    description = ""
+    default = "us-west-1a"
+}
+
+variable "private_subnet_availability_zone" {
+    type = string
+    description = ""
+    default = "us-west-1c"
+}
+
+#EKS Compute Vars
+
+variable "nodegroup_instance_types" {
+    type = list
+    description = ""
+    default = ["t2.micro"]
+}
+
+#AWS Credentials, CHANGE LATER
+
+variable "aws_access_key" {
+    type = string
+    description = ""
+    sensitive = true
+}
+
+variable "aws_secret_key" {
+    type = string
+    description = ""
+    sensitive = true
+}
+
+variable "aws_region" {
+    type = string
+    description = ""
+    sensitive = true
+}
