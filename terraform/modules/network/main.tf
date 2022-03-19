417694a6732ba19724460705ada32da1c7ef0372
Ricky-Lopez <rllopez718@gmail.com>
2022-03-18 20:59:12 -0400

    enabled dns service on vpc in terraform network module.

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -9,6 +9,7 @@ terraform {
 resource "aws_vpc" "main_vpc" {
   cidr_block           = var.VPC_cidr_block
   enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "RL-Aline-VPC"
   }
