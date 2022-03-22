be6f52477d3db93a4b237abec0e885b3b566a19c
EC2 Default User <ec2-user@ip-172-31-5-191.ec2.internal>
2022-03-22 04:37:27 +0000

    took out error cleanup  from shell script, should add later.

--- k8s/new-script.sh
+++ k8s/new-script.sh
@@ -1,28 +1,20 @@
-#!bin/bash
+#!/bin/bash
 
 #ensure that script runs in directory that it inhabits.
 
 # enable !! command completion for easier scripting.
 set -o history -o histexpand
 
-#exit when any command fails, and provide.
+#exit when any command fails 
 set -e
+set -o pipefail
 
-exit_on_error() {
-    exit_code=$1
-    last_command=${@:2}
-    if [ $exit_code -ne 0 ]; then
-        >&2 echo "\"${last_command}\" command failed with exit code ${exit_code}."
-    fi
-}
-
-trap "exit_on_error $? !!" EXIT
 
 #establish env vars in environment.
 source .env
 
 #inject env vars into cluster configuration, and apply.
-envsubst < cluster.yaml | eskctl create cluster -f -
+envsubst < cluster.yaml | eksctl create cluster -f -
 
 
 #create environment resources. 
@@ -41,3 +33,4 @@ envsubst < gateway/gateway-configmap.yaml | kubectl apply -f -
 
 #create gateway deployment and service.
 kubectl apply -f gateway/gateway.yaml && kubectl apply -f gateway/gateway-service.yaml
+
