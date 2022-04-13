0175b0234a0c080f36ad977b2fdf633360be24b3
Ricky-Lopez <rllopez718@gmail.com>
2022-04-13 00:54:48 -0400

    cleaned up kubetl.sh, also added tasks to install and run mysql.

--- ansible/playbooks/kubectl.sh
+++ ansible/playbooks/kubectl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Needs to run as root to work properly!
+
 #Retrieve kubectl binary.
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
 
