e052860752da0c682d2d18500d158e16d5c30f32
Ricky-Lopez <rllopez718@gmail.com>
2022-04-12 23:31:27 -0400

    added kubectl.sh and block to use script in bastion.yaml.

---
+++ ansible/playbooks/kubectl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Retrieve kubectl binary.
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
+
+#Apply execute permissions to binary.
+chmod +x ./kubectl
+
+#Copy kubectl binary to binary folder.
+cp ./kubectl $HOME/bin/kubectl
